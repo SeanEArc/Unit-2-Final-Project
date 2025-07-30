680aa83073992a8087ffd164ed35a521d72e2ecd
SeanEArc <seanearcaya@gmail.com>
2025-07-30 16:54:37 -0500

    Worked on NavBar functionality and LoginPage component.

--- React-Final-copy/src/components/auth/LoginPage.jsx
+++ React-Final-copy/src/components/auth/LoginPage.jsx
@@ -14,6 +14,7 @@ const LoginPage = () => {
       const [error, setError] = useState(null);
       const [showNavBar, setShowNavBar] = useState(true);
 
+      
       // useNavigate to redirect after login
       const navigate = useNavigate();
 
